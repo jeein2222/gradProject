d27fa6ecef81587ed138eb9caf7d964aa5144a22
jeein <jeein2726@naver.com>
2022-03-26 13:52:42 +0900

    Modified templates to display musics from database.

--- lyricmd/views.py
+++ lyricmd/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Music
 
 # Create your views here.
 def music_list(request):
-    return render(request,'lyricmd/music_list.html',{})
+    musics=Music.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'lyricmd/music_list.html',{'musics':musics})
